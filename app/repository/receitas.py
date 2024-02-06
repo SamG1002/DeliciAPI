2e9fca39f0038568b9a758c2d7f6785c838eefe0
SamG1002 <samuelguerra.dev@gmail.com>
2024-02-05 21:56:16 -0300

    :zap: Transformando as funções em assincronas

--- app/repository/receitas.py
+++ app/repository/receitas.py
@@ -3,22 +3,22 @@ from pymongo.collection import Collection
 from app.models.receita import Receita, Secao
 from typing import List
 
-def get_receitas(collection: Collection):
+async def get_receitas(collection: Collection):
     return list(collection.find())
 
-def get_receita_by_id(collection: Collection, receita_id: str):
+async def get_receita_by_id(collection: Collection, receita_id: str):
     return collection.find_one({"_id": receita_id })
 
-def add_receita(collection: Collection, receita: Receita):
+async def add_receita(collection: Collection, receita: Receita):
     return collection.insert_one(receita.dict())
 
-def update_receita(collection: Collection, receita_id: str, receita: Receita):
+async def update_receita(collection: Collection, receita_id: str, receita: Receita):
     return collection.update_one({"_id": receita_id}, {"$set": receita.dict()})
 
-def delete_receita(collection: Collection, receita_id: str):
+async def delete_receita(collection: Collection, receita_id: str):
     return collection.delete_one({"_id": receita_id})
 
-def get_receita_by_ingrediente(collection: Collection, ingrediente: list):
+async def get_receita_by_ingrediente(collection: Collection, ingrediente: list):
     filtro = f".*{'|'.join(ingrediente)}.*"
     query = {
                 "secao.nome": " Ingredientes",
@@ -26,7 +26,7 @@ def get_receita_by_ingrediente(collection: Collection, ingrediente: list):
             }
     return list(collection.find(query))
 
-def get_receita_by_ingredientes(collection: Collection, ingredientes: list):
+async def get_receita_by_ingredientes(collection: Collection, ingredientes: list):
     filtro = "(?=.*" + ")(?=.*".join(ingredientes) + ")"
     query = {
                 "secao.nome": " Ingredientes",
@@ -34,7 +34,7 @@ def get_receita_by_ingredientes(collection: Collection, ingredientes: list):
             }
     return list(collection.find(query))
 
-def add_secao(collection: Collection, receita_id: str, secao: Secao):
+async def add_secao(collection: Collection, receita_id: str, secao: Secao):
 
     filtro = {"_id": receita_id, 'secao.nome': secao.nome}
     if collection.find_one({"_id": receita_id}):
--- app/routers/receitas.py
+++ app/routers/receitas.py
@@ -15,66 +15,66 @@ Page = Page.with_custom_options(
 )
 
 @router.get("/")
-def welcome():
+async def welcome():
     return 'Bem vindo ao DeliciAPI!'
 
 # Buscar todas as receitas
 @router.get("/receitas/", response_model=Page[Receita])
-def listar_receitas():
-    return paginate(get_receitas(Database.get_collection("receitas")))
+async def listar_receitas():
+    return paginate(await get_receitas(Database.get_collection("receitas")))
 
 # Buscar receita pelo id
 @router.get("/receitas/{receita_id}", response_model=Receita)
-def obter_receita(receita_id: str):
-    receita = get_receita_by_id(Database.get_collection("receitas"), ObjectId(receita_id))
+async def obter_receita(receita_id: str):
+    receita = await get_receita_by_id(Database.get_collection("receitas"), ObjectId(receita_id))
     if receita:
         return receita
     raise HTTPException(status_code=404, detail="Receita não encontrada")
 
 # Adicionar uma receita nova
-@router.post("/receitas/", response_model=Receita)
-def adicionar_receita(receita: Receita):
-    response = add_receita(Database.get_collection("receitas"), receita)
+@router.post("/receitas/")
+async def adicionar_receita(receita: Receita):
+    response = await add_receita(Database.get_collection("receitas"), receita)
     if response:
         return {"message": "Receita adicionada com sucesso", "status_code": status.HTTP_200_OK}
 
 
 # Editar uma receita existente pelo id
-@router.put("/receitas/{receita_id}", response_model=Receita)
-def atualizar_receita(receita_id: str, receita: Receita):
-    response = update_receita(Database.get_collection("receitas"), ObjectId(receita_id), receita)
+@router.put("/receitas/{receita_id}")
+async def atualizar_receita(receita_id: str, receita: Receita):
+    response =  await update_receita(Database.get_collection("receitas"), ObjectId(receita_id), receita)
     if response:
         return {"message": "Receita atualizada com sucesso", "status_code": status.HTTP_200_OK}
 
 
 # Apagar uma receita pelo id
 @router.delete("/receitas/{receita_id}", response_model=dict)
-def excluir_receita(receita_id: str):
-    response = delete_receita(Database.get_collection("receitas"), ObjectId(receita_id))
+async def excluir_receita(receita_id: str):
+    response = await delete_receita(Database.get_collection("receitas"), ObjectId(receita_id))
     if response:
         return {"message": "Receita excluida com sucesso", "status_code": status.HTTP_200_OK}
 
 
 # Buscar receitas que contenham algum dos ingredientes
 @router.get("/receitas/one_ingrediente/{ingredientes}", response_model=Page[Receita])
-def busca_por_ingrediente(ingredientes: str):
-    receitas = get_receita_by_ingrediente(Database.get_collection("receitas"), ingredientes.split(",") )
+async def busca_por_ingrediente(ingredientes: str):
+    receitas = await get_receita_by_ingrediente(Database.get_collection("receitas"), ingredientes.split(",") )
     if receitas:
         return paginate(receitas)
     raise HTTPException(status_code=404, detail="Receita não encontrada")
 
 # Buscar receitas que contenham todos os ingredientes
 @router.get("/receitas/all_ingredientes/{ingredientes}", response_model=Page[Receita])
-def busca_por_ingrediente(ingredientes: str):
-    receitas = get_receita_by_ingredientes(Database.get_collection("receitas"), ingredientes.split(",") )
+async def busca_por_ingrediente(ingredientes: str):
+    receitas = await get_receita_by_ingredientes(Database.get_collection("receitas"), ingredientes.split(",") )
     if receitas:
         return paginate(receitas)
     raise HTTPException(status_code=404, detail="Receita não encontrada")
 
 # Adicionar secao em uma receita (como comentario, avaliacao, dificuldade, etc)
 @router.put("/receitas/secao/{receita_id}")
-def adicionar_secao(receita_id: str, secao: Secao):
-    receita = add_secao(Database.get_collection("receitas"), ObjectId(receita_id), secao)
+async def adicionar_secao(receita_id: str, secao: Secao):
+    receita = await add_secao(Database.get_collection("receitas"), ObjectId(receita_id), secao)
     if receita:
         return {"message": "Seção adicionada", "status_code": status.HTTP_200_OK}
     raise HTTPException(status_code=404, detail="Receita não encontrada")
